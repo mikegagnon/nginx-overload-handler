99ef0dcda7f1341bd61c9a72366c38482e06204c
Michael N. Gagnon <mikegagnon@gmail.com>
2012-05-09 22:46:11 -0700

    fixed bug in install_mediawiki.sh

--- apps/mediawiki_app/install_mediawiki.sh
+++ apps/mediawiki_app/install_mediawiki.sh
@@ -44,7 +44,7 @@ php $INSTALL_MEDIA_WIKI_PATH/maintenance/install.php \
     --pass "$MEDIAWIKI_PASSWORD" \
     --scriptpath "" \
     --dbuser root \
-    --dbpass "$MYSQL_PASSWORD"
+    --dbpass "$MYSQL_PASSWORD" \
     --server "$MEDIAWIKI_ROOT_URL"
 
 echo -e '$wgMaxUploadSize = 1024 * 1024 * 100;\n' >> $INSTALL_MEDIA_WIKI_PATH/LocalSettings.php
