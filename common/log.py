833e69d72dc398f84ef133e0d16f0c87c49db730
Michael N. Gagnon <mikegagnon@gmail.com>
2012-05-10 10:04:43 -0700

    new logging module

--- common/log.py
+++ common/log.py
@@ -18,4 +18,59 @@
 #
 
 import logging
+import logging.handlers
+import sys
+import os
+
+DIRNAME = os.path.dirname(os.path.realpath(__file__))
+LOGDIR = os.path.join(DIRNAME, "..", "log")
+LOGDIR = os.path.realpath(LOGDIR)
+
+MAX_LOGFILE_BYTES = 1024 * 1024 * 1 # 1 MB
+BACKUP_COUNT = 5
+
+FORMATTER_STDERR = logging.Formatter("%(asctime)s - %(levelname)10s - %(process)d - %(filename)s:%(funcName)s - %(message)s")
+FORMATTER_LOGFILE = FORMATTER_STDERR
+
+def getLogger(stderr=None, logfile=None, name=None):
+    '''to log to stderr set stderr = a level from logging
+    to log to ../log/foo.log set logfile = a level from logging and
+    set name = "foo"'''
+
+    if name == None:
+        # TODO: Get the filename of the caller
+        name = "default"
+    logger = logging.getLogger(name)
+    if not (stderr != None or logfile != None):
+        raise ValueError("You must set at least one of stderr or logfile to a logging level")
+    handlers = []
+    if stderr != None:
+        handler = logging.StreamHandler(sys.stderr)
+        handler.setLevel(stderr)
+        handler.setFormatter(FORMATTER_STDERR)
+        handlers.append(handler)
+    if logfile != None:
+        filename = os.path.join(LOGDIR, name) + ".log"
+        handler = logging.handlers.RotatingFileHandler(
+            filename,
+            mode = 'a',
+            maxBytes = MAX_LOGFILE_BYTES,
+            backupCount = BACKUP_COUNT)
+        handler.setLevel(logfile)
+        handler.setFormatter(FORMATTER_LOGFILE)
+        handlers.append(handler)
+    min_level = min([handler.level for handler in handlers])
+    for handler in handlers:
+        logger.addHandler(handler)
+    logger.setLevel(min_level)
+    logger.debug("New logger instance")
+    return logger
+
+if __name__ == "__main__":
+    log = getLogger(stderr=logging.INFO,logfile=logging.DEBUG)
+    log.critical("test")
+    log.error("test")
+    log.warning("test")
+    log.info("test")
+    log.debug("test")
 
