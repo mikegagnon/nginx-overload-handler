151efc39eeb3e05e6a35daf2f9fa89fd6410a348
Michael N. Gagnon <mikegagnon@gmail.com>
2012-06-02 18:54:22 -0700

    puzzle now reports progress

--- nginx_doorman/puzzle_static/puzzle.js
+++ nginx_doorman/puzzle_static/puzzle.js
@@ -57,35 +57,25 @@ function redirect(request, key) {
 
 // solves the puzzle (in bursts, with sleep time inbetween bursts)
 // then calls redirect with the solution.
-// TODO: update user with progress bar or something
-function solve_puzzle(request, y, x, bits, burst_len, sleep_time) {
+function solve_puzzle(request, y, x, tries, bits, burst_len, sleep_time) {
     var hash_x;
     for (i = 0; i < burst_len; i++) {
-        //hash_x = hex_sha256(x);
         hash_x = hex_md5(x);
         if (hash_x == y) {
             redirect(request, x);
         }
         x = increment(x);
     }
-    var func = function() {
-        solve_puzzle(request, y, x, bits, burst_len, sleep_time);
-    };
-    setTimeout(func, sleep_time);
-}
-
-function solve_puzzle_old(y, trunc_x, bits, burst_len, sleep_time) {
-    var x = trunc_x;
-    var hash_x;
-    while (true) {
-        for (i = 0; i < burst_len; i++) {
-            //hash_x = hex_sha256(x);
-            hash_x = hex_md5(x);
-            if (hash_x == y) {
-                return x;
-            }
-            x = increment(x);
+    tries += burst_len;
+    percent_done = Math.floor((tries / Math.pow(2, bits)) * 100.0);
+    if (percent_done <= 100) {
+        document.getElementById("redirect_status").innerHTML = "" + Math.floor((tries / Math.pow(2, bits)) * 100.0);
+        var func = function() {
+            solve_puzzle(request, y, x, tries, bits, burst_len, sleep_time);
         }
+        setTimeout(func, sleep_time);
+    } else {
+        document.getElementById("redirect_status").innerHTML = "ERROR";
     }
 }
 
