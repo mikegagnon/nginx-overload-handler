fbe19122675c8260a985e067ffbc02a5572fc928
Michael N. Gagnon <mikegagnon@gmail.com>
2012-06-16 13:50:23 -0700

    new dummy vuln for osqa

---
+++ apps/osqa_app/install_dummy_vuln.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# Copyright 2012 HellaSec, LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obCONFIG_INSTALLED_BACKUPtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+# ==== install_dummy_vuln.sh for OSQA ====
+#
+# Installs (1) a dummy high-density vulnerability into the OSQA installation,
+# and (2) a dummy XSS vuln.
+#
+# USAGE: sudo ./install_dummy_vuln.sh
+#
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/env.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $INSTALL_OSQA_PATH
+patch -p1 < "$DIR/osqa_dummy_vuln.patch"
+
