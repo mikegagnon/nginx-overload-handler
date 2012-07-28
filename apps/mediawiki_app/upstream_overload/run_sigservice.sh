629fc202d9ebbe919b6ac804f92184c3e3701fd1
Michael N. Gagnon <mikegagnon@gmail.com>
2012-07-28 11:19:43 -0700

    added run_sigservice

---
+++ apps/mediawiki_app/upstream_overload/run_sigservice.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# Copyright 2012 HellaSec, LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obCONFIG_INSTALLED_BACKUPtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+# ==== run_sigservice.sh for mediawiki_app ====
+#
+# USAGE: sudo ./run_sigservice.sh &
+#
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/../../../dependencies/env.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+$SUDO -i -u nginx_user $DIR/../../../sig_service/sigservice.py --stderr off --logfile DEBUG
+
