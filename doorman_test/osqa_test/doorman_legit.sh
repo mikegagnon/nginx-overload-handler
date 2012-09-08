4fb1e61cafff9f2226eccfb41abe6a7c50366aac
Mike Gagnon <mike@hellasec.com>
2012-09-08 00:47:05 -0500

    osqa_test

---
+++ doorman_test/osqa_test/doorman_legit.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+# Copyright 2012 HellaSec, LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+# ==== doorman_legit.sh for mediawiki ====
+#
+# Example usage: ./doorman_legit.sh --id EXPERIMENT_NAME --server SERVER --url FILE --rate 30 --duration 20
+#
+
+# $DIR is the absolute path for the directory containing this bash script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/../env.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/env.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+FILENAME_BASE=`echo "legit_$*" | sed 's/\s/_/g' | tr '/' '_'`
+TRACE_FILENAME="$DIR/results/$FILENAME_BASE.csv"
+SUMMARY_FILENAME="$DIR/results/$FILENAME_BASE.json"
+echo $SUMMARY_FILENAME
+
+$DOORMAN_LEGIT \
+    --stderr ERROR \
+    --url-file $DIR/trace01.txt \
+    --poisson \
+    --timeout 20 \
+    --trace-filename $TRACE_FILENAME \
+    --regex "OSQA" \
+    $*
+
+cat $TRACE_FILENAME | $DOORMAN_ANALYZE > $SUMMARY_FILENAME
+
+echo "done legit"
+
