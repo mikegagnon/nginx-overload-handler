f5a358cf20be3238c0d5d6ad018117824ddb1c10
Michael N. Gagnon <mikegagnon@gmail.com>
2012-09-19 20:49:44 -0700

    Update apps/mediawiki_app/dummy_vuln_sql.php

--- apps/mediawiki_app/dummy_vuln_sql.php
+++ apps/mediawiki_app/dummy_vuln_sql.php
@@ -1,5 +1,7 @@
 <?php
 
+# idea from http://stackoverflow.com/questions/3892374/how-to-intentionally-create-a-long-running-mysql-query
+
 $dbname = "my_wiki";
 $query = "select benchmark(9999999999, md5('this will be hashed many times'));";
 $env_user = getenv('MYSQL_USER');
